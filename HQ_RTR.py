c5aace714ba724276daffe4db4f979580f5a4256
Pow(i,x) <mm5718898@gmail.com>
2025-10-31 13:00:45 +0500

    Add files via upload

---
+++ HQ_RTR.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+import os, subprocess
+
+#Настройка портов
+res = subprocess.run(["ip", "-c", "--br", "a"], capture_output=True, text=True, encoding='utf-8')
+res=res.stdout.split()
+ports=[iface for iface in res if iface.startswith("ens")]
+os.system(f"mkdir /etc/net/ifaces/{ports[1]}")
+os.system(f"mkdir /etc/net/ifaces/{ports[2]}")
+os.system(f"echo '192.168.1.2/24' > /etc/net/ifaces/{ports[0]}/ipv4address")
+os.system(f"echo '192.168.3.1/24' > /etc/net/ifaces/{ports[1]}/ipv4address")
+os.system(f"echo '192.168.4.1/24' > /etc/net/ifaces/{ports[2]}/ipv4address")
+os.system(f"cat << OEF > /etc/net/ifaces/{ports[0]}/options\n BOOTPROTO=static\n TYPE=eth\n CONFIG_WIRELESS=no\n SYSTEMD_BOOTPROTO=static\n CONFIG_IPV4=yes\n DISABLED=no\n NM_CONTROLLED=no\n SYSTEMD_CONTROLLED=no")
+os.system(f"cat << OEF > /etc/net/ifaces/{ports[1]}/options\n BOOTPROTO=static\n TYPE=eth\n CONFIG_WIRELESS=no\n SYSTEMD_BOOTPROTO=static\n CONFIG_IPV4=yes\n DISABLED=no\n NM_CONTROLLED=no\n SYSTEMD_CONTROLLED=no")
+os.system(f"cat << OEF > /etc/net/ifaces/{ports[2]}/options\n BOOTPROTO=static\n TYPE=eth\n CONFIG_WIRELESS=no\n SYSTEMD_BOOTPROTO=static\n CONFIG_IPV4=yes\n DISABLED=no\n NM_CONTROLLED=no\n SYSTEMD_CONTROLLED=no")
+
+#Мелкая настройка
+os.system("hostnamectl set-hostname HQ-RTR")
+os.system("sysctl -w net.ipv4.ip_forward=1")
+os.system("systemctl restart network")
+
+#Настройка Firewall
+os.system("apt-get update && apt-get -y install firewalld && systemctl enable --now firewalld")
+os.system(f"firewall-cmd --permanent --zone=public --add-interface={ports[0]}")
+os.system(f"firewall-cmd --permanent --zone=trusted --add-interface={ports[1]}")
+os.system(f"firewall-cmd --permanent --zone=trusted --add-interface={ports[2]}")
+os.system("firewall-cmd --permanent --zone=public --add-masquerade")
+os.system("firewall-cmd --complete-reload")
+
+#Настройка DHCP
+os.system("apt-get install dhcp-server")
+os.system("cp -pf dhcpd.conf /etc/dhcp")
+os.system(f"echo DHCPDARGS={ports[1]} > /etc/sysconfig/dhcpd")
+os.system("systemctl enable dhcpd && systemctl start dhcpd")
